2152263aeb6ca5967b3aa4aed6e8865249352f13
budildav <Davebudil@gmail.com>
2017-10-19 12:27:08 +0200

    Board metody

--- src/budha/thedrake/Board.java
+++ src/budha/thedrake/Board.java
@@ -3,22 +3,48 @@ package budha.thedrake;
 /**
  * Created by havrda on 16/10/2017.
  */
-/* TODO
 public class Board {
+    private final Tile playingBoard[][];
+    private final int boardDimension;
     // Konstruktor. Vytvoří čtvercovou hrací desku zadaného rozměru se specefikovanými dlaždicemi.
     // Všechny ostatní dlažice se berou jako prázdné.
-    public Board(int dimension, Tile... tiles)
+    public Board(int dimension, Tile... tiles){
+        this.playingBoard = new Tile[dimension][dimension];
+        this.boardDimension = dimension;
+
+        for(Tile t : tiles){
+
+        }
+    }
 
     // Rozměr hrací desky
-    public int dimension()
+    public int dimension(){
+        return this.boardDimension;
+    }
 
     // Vrací dlaždici na zvolené pozici. Pokud je pozice mimo desku, vyhazuje IllegalArgumentException
-    public Tile tileAt(TilePosition position)
+    public Tile tileAt(TilePosition position){
+        if(!contains(position))
+            throw IllegalArgumentException;
+    }
 
     // Ověřuje, že pozice se nachází na hrací desce
-    public boolean contains(TilePosition... positions)
+    public boolean contains(TilePosition... positions){
+
+        for(TilePosition t : positions){
+            if(t.column() > boardDimension)
+                return false;
+            else if(t.row() > boardDimension)
+                return false;
+        }
+        return true;
+    }
 
     // Vytváří novou hrací desku s novými dlaždicemi
-    public Board withTiles(Tile... tiles)
+    public Board withTiles(Tile... tiles){
+
+
+
+    }
 }
-*/
+
