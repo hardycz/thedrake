1d642c4415b5537e2232f6df94ea433075d0d757
budildav <Davebudil@gmail.com>
2017-10-31 14:08:24 +0100

    Mensi uprava, jeste zbyva implementovat StrikeAction

--- src/budha/thedrake/StrikeAction.java
+++ src/budha/thedrake/StrikeAction.java
@@ -10,10 +10,8 @@ inspirovat se SlideAction a ShiftAction
 
 public class StrikeAction implements TroopAction {
 
-
-
     @Override
     public List<BoardChange> changesFrom(TilePosition origin, PlayingSide side, Board board) {
-        return null;
+        return null; // tmp
     }
 }
--- src/budha/thedrake/TroopInfo.java
+++ src/budha/thedrake/TroopInfo.java
@@ -22,12 +22,12 @@ public class TroopInfo {
         this.backActions = backActions;
     }
     // Konstruktor, který nastaví lícový i rubový pivot na stejnou hodnotu
-    public TroopInfo(String name, Offset2D pivot) {
-    this(name, pivot, pivot);
+    public TroopInfo(String name, Offset2D pivot, List<TroopAction> frontActions, List<TroopAction> backActions>) {
+    this(name, pivot, pivot, frontActions, backActions);
 }
     // Konstruktor, který nastaví lícový i rubový pivot na hodnotu [1, 1]
-    public TroopInfo(String name){
-    this(name, new Offset2D(1,1));
+    public TroopInfo(String name, List<TroopAction> frontActions, List<TroopAction> backActions){
+    this(name, new Offset2D(1,1), frontActions, backActions);
 }
     //Přidejte do třídy metod, která vrací akce pro zadanou stranu jednotky.
     public List<TroopAction> actions(TroopFace face){
