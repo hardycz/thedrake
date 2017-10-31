a3eae7e028f2ee7860e7d8e7c30860a12a17cb5b
budildav <Davebudil@gmail.com>
2017-10-31 20:24:16 +0100

    Prepsal jsem capturedTroops -> uz prochazi. Nefunguje changesTest, boardtest a strikeActionTest na 100%

--- src/budha/thedrake/CapturedTroops.java
+++ src/budha/thedrake/CapturedTroops.java
@@ -1,40 +1,55 @@
 package budha.thedrake;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CapturedTroops {
-    private List<TroopInfo> troopsPlayerOne;
-    private List<TroopInfo> troopsPlayerTwo;
+    private final LinkedList<TroopInfo> capturedBlue;
+    private final LinkedList<TroopInfo> capturedOrange;
 
     // Konstruktor vytvářející prázdné seznamy
-    public CapturedTroops() {
-        troopsPlayerOne = new ArrayList<TroopInfo>();
-        troopsPlayerTwo = new ArrayList<TroopInfo>();
+    public CapturedTroops(){
+        capturedBlue = new LinkedList<>();
+        capturedOrange = new LinkedList<>();
+    }
+
+    public CapturedTroops(CapturedTroops other, TroopInfo info, PlayingSide side) {
+
+        capturedBlue = new LinkedList<>();
+        capturedOrange = new LinkedList<>();
+
+        capturedBlue.addAll(other.capturedBlue);
+        capturedOrange.addAll(other.capturedOrange);
+
+        if(side == PlayingSide.BLUE){
+            capturedBlue.addFirst(info);
+        } else {
+            capturedOrange.addFirst(info);
+        }
+
     }
 
     // Vrací seznam zajatých jednotek pro daného hráče
     public List<TroopInfo> troops(PlayingSide side) {
-        return (side == PlayingSide.ORANGE) ?
-                Collections.unmodifiableList(troopsPlayerOne):
-                Collections.unmodifiableList(troopsPlayerTwo);
+
+        if(side == PlayingSide.BLUE){
+            List<TroopInfo> unmodifiableList = Collections.unmodifiableList(capturedBlue);
+            return unmodifiableList;
+
+        } else {
+            List<TroopInfo> unmodifiableList = Collections.unmodifiableList(capturedOrange);
+            return unmodifiableList;
+        }
     }
 
     // Přidává nově zajatou jednotku na začátek seznamu zajatých jednotek daného hráče.
-    public CapturedTroops withTroop(PlayingSide side, TroopInfo info) {
-        CapturedTroops newCapturedTroops = new CapturedTroops();
-        newCapturedTroops.cloneCapturedTroops(this.troopsPlayerOne, this.troopsPlayerTwo);
-        newCapturedTroops.addTroopToList(side, info);
-        return newCapturedTroops;
-    }
-    private void cloneCapturedTroops(List<TroopInfo> oldTroopsPlayerOne, List<TroopInfo> oldTroopsPlayerTwo) {
-        Collections.copy(this.troopsPlayerOne, oldTroopsPlayerOne);
-        Collections.copy(this.troopsPlayerTwo, oldTroopsPlayerTwo);
-    }
+    public CapturedTroops withTroop(PlayingSide side, TroopInfo info){
 
-    private void addTroopToList(PlayingSide side, TroopInfo info){
-        if(side == PlayingSide.ORANGE) troopsPlayerOne.add(info);
-            else troopsPlayerTwo.add(info);
+        return new CapturedTroops(this, info, side);
+
     }
 }
