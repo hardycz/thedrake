39bab3da8aed592bfe81b0d4284af757788c2287
budildav <Davebudil@gmail.com>
2017-11-04 09:41:55 +0100

    MiddleGameState -> in progress

--- src/budha/thedrake/MiddleGameState.java
+++ src/budha/thedrake/MiddleGameState.java
@@ -27,23 +27,56 @@ public class MiddleGameState extends BaseGameState {
 
     @Override
     public List<Move> allMoves() {
-        //TODO
-        // Zde doplňte vlastní implementaci
-        return null;
+        List<Move> result = new ArrayList<>();
+        for(Tile tile : board()) {
+            result.addAll(boardMoves(tile.position()));
+        }
+        return result;
     }
 
     @Override
     public List<Move> boardMoves(TilePosition position) {
-        //TODO
-        // Zde doplňte vlastní implementaci
-        return null;
+        List<Move> result = new ArrayList<>();
+        PlayingSide side = sideOnTurn();
+        Tile tile = board().tileAt(position);
+        Troop troop;
+        List<TroopAction> actions;
+        List<BoardChange> boardChanges;
+
+        if(!tile.hasTroop()) {
+            return Collections.emptyList();
+        } else {
+            troop = tile.troop();
+            if(troop.side() != side) {
+                return Collections.emptyList();
+            }
+        }
+
+        actions = troop.info().actions(troop.face());
+        boardChanges = new ArrayList<>();
+
+        for(TroopAction action : actions) {
+            boardChanges = action.changesFrom(position, side, board());
+            for(BoardChange change : boardChanges){
+                result.add(new BoardMove(this, change));
+            }
+        }
+        return result;
     }
 
     @Override
     public List<Move> stackMoves() {
-        //TODO
-        // Zde doplňte vlastní implementaci
-        return null;
+        List<Move> result = new ArrayList<>();
+        PlayingSide side = sideOnTurn();
+        Board board = board();
+        Troop troop = troopStacks().peek(sideOnTurn());
+
+        for(Tile target : board) {
+            if(!target.hasTroop()) {
+                result.add(new PlaceFromStack(this, target.position()));
+            }
+        }
+        return result;
     }
 
     @Override
@@ -52,9 +85,22 @@ public class MiddleGameState extends BaseGameState {
     }
 
     public boolean canPlaceFromStack(TilePosition target) {
+        Troop troop = troopStacks().peek(sideOnTurn());
+        if(!board().canPlaceTo(troop, target)) {
+            return false;
+        }
+
         //TODO
-        // Zde doplňte vlastní implementaci
-        return true;
+
+            /*
+            boolean hasLeaderAsNeigbour = false;
+            hasLeaderAsNeigbour |= tryNeighbour(target, 0, 1);
+            hasLeaderAsNeigbour |= tryNeighbour(target, 0, -1);
+            hasLeaderAsNeigbour |= tryNeighbour(target, 1, 0);
+            hasLeaderAsNeigbour |= tryNeighbour(target, -1, 0);
+
+            return hasLeaderAsNeigbour;
+            */
     }
 
     public MiddleGameState placeFromStack(TilePosition target) {
